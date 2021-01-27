7ec5704bd9bf6dfa650a798d447d709602e1f713
matteron <accounts@mattia.id>
2021-01-26 22:43:24 -0500

    Simplified getting path

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const config = {
 	path: '/Users/mattia/Library/Mobile Documents/com~apple~CloudDocs/Links/',
 	data: 'links.json'
 }
+
+const arg = process.argv[2];
+if (arg) {
+	console.log(config[arg]);
+}
+
+module.exports = config;
